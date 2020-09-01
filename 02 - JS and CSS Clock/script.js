6899a8b05b489e27f474366e3c1dc545200b5b9f
Dan Valeroso <danlestervaleroso@gmail.com>
2020-09-01 22:40:24 +0800

    Fixed Transition Bugs

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -4,20 +4,25 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setDate(){
     const now = new Date();
-    
     const seconds = now.getSeconds();
     const minutes = now.getMinutes();
     const hours = now.getHours();
 
     const secondsDegrees = (seconds/60 * 360) + 90;
     const minutesDegrees = (minutes/60 * 360) + 90;
-    const hoursDegrees = (hours/60 * 360) + 90;
-
+    const hoursDegrees = (hours/12 * 360) + 90;
 
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     minHand.style.transform = `rotate(${minutesDegrees}deg)`;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-    console.log(seconds);
+
+    //fix transition bugs
+    if(seconds === 0) {
+        secondHand.style.transition = "none"; 
+    } else {
+        secondHand.style.transition = "all 0.05s cubic-bezier(0.1, 2.5, 0.58, 1)"; 
+    }
+
 }
 
 setInterval(setDate, 1000);
